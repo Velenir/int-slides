c917d13b195f9f9724a159a4fd3f73d5c9f4f524
Velenir <Velenir@users.noreply.github.com>
2017-05-18 10:26:48 +0300

    feat(range): Connected input range to red roller

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-console.log("Started App");
+const range = document.querySelector(".range");
+const input = range.querySelector(".range__input");
+
+input.addEventListener("change", function() {
+	console.log(this.value);
+	range.style.setProperty("--offset", this.value - 1);
+});
